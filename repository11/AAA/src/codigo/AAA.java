915aca7efb16be6925a4f2ead9e3f843ee2168ab
EPCVivares <94907335+EPCVivares@users.noreply.github.com>
2022-05-12 02:32:05 +0200

    Add files via upload

---
+++ repository11/AAA/src/codigo/AAA.java
@@ -0,0 +1,14 @@
+package codigo;
+
+public class AAA {
+
+	public static void main(String[] args) {
+		System.out.println("prueba github");
+
+		System.out.println("Sigo probando");
+		System.out.println("");
+
+
+	}
+
+}
